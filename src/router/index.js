8c73500c5f203c24d97dd77026bee212745ef3c1
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2024-06-13 20:07:24 +0500

    Setup vue-router and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,58 @@
 import { createRouter,createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'
-
+import DefaultLayout from '../components/DefaultLayout.vue';
+import GuestLayout from '../components/GuestLayout.vue';
+import Home from '../views/Home.vue';
+import Services from '../views/Services.vue';
+import CV from '../views/CV.vue';
+import Portfolio from '../views/Portfolio.vue';
+import Blog from '../views/Blog.vue';
+import Contact from '../views/Contact.vue';
 const routes = [
     {
         path: '/',
-        name: 'home',
-        component: Home
-    }
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/',
+                name: 'Home',
+                component: Home,
+            },
+            {
+                path: '/services',
+                name: 'Services',
+                component: Services,
+            },
+            {
+                path: '/cv',
+                name: 'CV',
+                component: CV,
+            },
+            {
+                path: '/portfolio',
+                name: 'Portfolio',
+                component: Portfolio,
+            },
+            {
+                path: '/blog',
+                name: 'Blog',
+                component: Blog,
+            },
+            {
+                path: '/contact',
+                name: 'Contact',
+                component: Contact,
+            },
+        ]
+    },
+    {
+        path: '/guest',
+        component: GuestLayout,
+        children: [
+    
+        ],
+    },
 ];
+
 const router  = createRouter({
     history: createWebHistory(),
     routes,
