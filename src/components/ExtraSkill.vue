3ccb82c22489b58364f2c47764a1f335601bcb97
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2024-06-14 22:19:14 +0500

    Create a component for extra skills

---
+++ src/components/ExtraSkill.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="extraSkills my-6 mx-auto">
+        <img class="extraSkillsIcon mr-3" src="../assets/icons/CV/icons.svg"/>
+        <span class="extraSkills">{{ name }}</span>
+    </div>
+</template>
+<script setup>
+
+const {name} = defineProps({
+    name: String,
+})
+
+</script>
