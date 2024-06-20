9de9d06057abdccafcb1c9e48b7cca4b3310a3e3
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2024-06-20 16:57:57 +0500

    Increasing width and height of Social media button

--- src/components/SocialMediaButton.vue
+++ src/components/SocialMediaButton.vue
@@ -1,6 +1,6 @@
 <template>
     <a :href="link" class="group">
-        <iconComponent class="bg-amber-400 fill-gray-800 flex justify-center items-center w-7 h-7 rounded-full mx-1 p-1 transition-all  group-hover:bg-amber-500 group-hover:scale-110 "/>
+        <iconComponent class="bg-amber-400 fill-gray-800 flex justify-center items-center w-10 h-10 rounded-full mx-1 p-1 transition-all  group-hover:bg-amber-500 group-hover:scale-110 "/>
     </a>
 </template>
 <script setup>
