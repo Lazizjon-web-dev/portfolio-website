a023d4333a016d0455f3e4d927f7c6bed33ffd80
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2024-07-10 19:56:11 +0500

    Create contact view

---
+++ src/components/ContactInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <label class="block text-color5 text-xl font-bold capitalize">
+            {{ label + (isRequired ? " (Required)" : "") }}
+        </label>
+        <input v-if="!isTextArea" class="w-11/12 mt-2.5 mb-6 p-4 bg-color7 border-none rounded-sm text-3xl" :type="type" :required="isRequired">
+        <textarea v-else class=" min-h-52 w-11/12 mt-2.5 mb-6 p-4 bg-color7 border-none rounded-sm text-3xl font-sans resize-none"></textarea>
+    </div>
+</template>
+<script setup>
+const { label, type, isRequired, isTextArea, value } = defineProps({
+    label: String,
+    type: String,
+    isRequired: Boolean,
+    isTextArea: Boolean,
+    value: String,
+})
+</script>
+<style lang="scss">
+
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,49 +1,55 @@
 <template>
-    <div class="w-full flex flex-col md:flex-row justify-between ">
+    <div class="w-full flex flex-col md:flex-row justify-between mx-4">
         <div class="box-border basis-3/5">
             <div class="inline-block m-5 mt-8 text-4xl font-bold capitalize text-center">send us your information</div>
-            <form action="#" class="bg-white mr-4 p-6">
+            <form action="#" class="bg-color8 box-border mr-4 p-6 mb-0 h-full">
                 <ContactInput v-for="(input,i) in inputForms" 
                     :key="i"
                     :label="input.label" 
+                    :type="input.type"
                     :is-required="input.isRequired"
                     :is-text-area="input.isTextArea"
                     :value="input.value"
                 />
-                <button class="bg-amber-400 text-lg font-bold border-none rounded-sm h-12 w-fit mt-2.5 mb-6 ml-5 py-2.5 px-8" type="submit" value="">SEND MESSAGE</button>
+                <button class="bg-primary text-lg font-bold border-none rounded-sm h-12 w-fit mt-2.5 mb-6 ml-5 py-2.5 px-8" type="submit" value="">SEND MESSAGE</button>
             </form>
         </div>
         <div class="basis-2/5">
             <div class="m-5 mt-8 text-4xl font-bold capitalize text-center"> my contact info</div>
-            <div class="w-full h-[879.2px] flex flex-col justify-between bg-gray-200">
+            <div class="w-full h-full flex flex-col justify-between bg-gray-200">
                 <ContactCard v-for="({icon,contactInfos},i) in ContactFields" :key="i" :icon="icon" :contact-infos="contactInfos"/>
             </div>
         </div>
     </div>
-    <Map class="block w-full md:w-5/6 md:mx-auto  h-96 my-20 border-none"/>
+    <Map/>
 </template>
 <script setup>
+// Import components
 import Map from "../components/Map.vue";
 import ContactInput from "../components/ContactInput.vue";
+import ContactCard from "../components/ContactCard.vue";
 
+// Import icons
 import MailIcon from "../components/icons/Contact/MailIcon.vue";
 import LocationIcon from "../components/icons/Contact/LocationIcon.vue";
 import MobileIcon from "../components/icons/Contact/MobileIcon.vue";
-import ContactCard from "../components/ContactCard.vue";
 
 const inputForms = [
     {
         label: 'your full name',
+        type: 'text',
         isRequired: true,
         value: '',
     },
     {
         label: 'your email',
+        type: 'email',
         isRequired: true,
         value: '',
     },
     {
         label: 'subject',
+        type: 'text',
         isRequired: false,
         value: '',
     },
@@ -58,91 +64,28 @@ const inputForms = [
 const ContactFields = [
     {
         icon: LocationIcon,
-        contactInfos: [
-        {
-                label: 'country',
-                value: 'Uzbekistan',
+        contactInfos: {
+            country: "Uzbekistan",
+            city: "Navoi",
+            street: 'Makhmud',
             },
-            {
-                label: 'city',
-                value: 'Navoi',
-            },
-            {
-                label: 'street',
-                value: 'Makhmud',
-            },
-        ],
     },
     {
         icon: MailIcon,
-        contactInfos: [
-            {
-                label: 'country',
-                value: 'Uzbekistan',
+        contactInfos: {
+            email: "azizjonovlazizjon64@gmail.com",
+            skype: "@lazizjon",
+            telegram: '@Lazizjon_Azizjonov',
             },
-            {
-                label: 'city',
-                value: 'Navoi',
-            },
-            {
-                label: 'street',
-                value: 'Makhmud',
-            },
-        ],
     },
     {
         icon: MobileIcon,
-        contactInfos: [
-        {
-                label: 'country',
-                value: 'Uzbekistan',
+        contactInfos: {
+            personal: "+998 91 308 2006",
+            city: "Navoi",
+            street: 'Makhmud',
             },
-            {
-                label: 'city',
-                value: 'Navoi',
-            },
-            {
-                label: 'street',
-                value: 'Makhmud',
-            },
-        ],
     }
-]
+];
 
 </script>
-<style lang="scss">
-
-label{
-    color:var(--color8);
-    font-weight:700;
-    font-size:20px;
-}
-#contactMessage{
-    width:96%;
-    min-height: 210px;
-    resize: none;
-    font-family: Roboto,sans-serif;
-    padding: 10px;
-}
-.contactInformation{
-    background-color: var(--color4) !important;
-    flex:3;
-}
-.contactCard{
-    min-height: 210px;
-    padding:15px 25px;
-    background-color: var(--color3);
-}
-.contactInformationIcon{
-    display:block;
-    margin:5px auto 30px;
-    background-color:var(--color2);
-    padding:5px;
-    border-radius:50%;
-}
-.contactInfoPropertyDiv{
-    margin:20px 5px;
-    font-size: 18px;
-    font-weight: 500;
-}
-</style>
